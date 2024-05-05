12473780c858c275caa550b0ce3414a7af4da2c2
Sandy_Wu <47694085+Sandy58061355@users.noreply.github.com>
2024-05-05 21:15:41 +0800

    音樂生成頁面RWD+導航欄功能

---
+++ music-generate.js
@@ -0,0 +1,32 @@
+// 漢堡
+window.addEventListener("load", function () {
+    //------btnHam漢堡按鈕被點按時
+    document.getElementById("btnHam").onclick = function () {
+        //取得選單的參考
+        let menu = document.getElementById("menu");
+        menu.classList.toggle("hidden");
+    };
+}, false);
+
+//搜索框(不得輸入空值)
+let searchInput = document.getElementById('music-generate-search');
+let musicStyleBtn = document.querySelector('.music-style-search-btn');
+var searchLinkMusicGenerate = document.getElementById('search-link-mg');
+
+searchInput.addEventListener('input', function () {
+    var inputVal = searchInput.value.trim();
+    if (inputVal) {
+        musicStyleBtn.classList.remove('disabled');
+        searchLinkMusicGenerate.href = 'music-generate.html?search=' + encodeURIComponent(inputVal);
+    } else {
+        musicStyleBtn.classList.add('disabled');
+        searchLinkMusicGenerate.href = '#';
+    }
+});
+
+musicStyleBtn.addEventListener('click', function (e) {
+    if (musicStyleBtn.classList.contains('disabled')) {
+        e.preventDefault(); // 阻止跳轉
+        alert('請輸入搜索內容');
+    }
+});
